59ee42ca58889e01adc37b565d986dab1a11359a
MikeKutz <mkutz1492@gmail.com>
2020-09-22 16:31:43 -0400

    added TE_SYNTAX package to install script.

--- install.sql
+++ install.sql
@@ -1,19 +1,23 @@
 Prompt creating table
 @@TE_TEMPLATES.sql
 
-Prompt create template package
-@@tePLSQL.pks
-@@tePLSQL.pkb
+Prompt creating Syntax checking package
+@@TE_SYNTAX.pks
+@@TE_SYNTAX.pkb
 
 Prompt create template api package
 @@TE_TEMPLATES_API.pks
 @@TE_TEMPLATES_API.pkb
 
+Prompt create template package
+@@tePLSQL.pks
+@@tePLSQL.pkb
+
 Prompt installing default helper templates
 @@TE_DEFAULT_HELPER_TEMPLATES.pks
 @@TE_DEFAULT_HELPER_TEMPLATES.pkb
 
-exec te_derault_helper_templates.install_templates;
+exec te_default_helper_templates.install_templates;
 commit;
 
 
